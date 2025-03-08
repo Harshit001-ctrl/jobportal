507cbe273c47eaca0e6f432179ae26513772b23d
Harshit1234332 <harshitrao.dev@gmail.com>
2025-03-08 14:26:58 +0530

    work

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -62,7 +62,7 @@ const Login = () => {
     <div className="flex min-h-screen bg-gray-50">
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <h2 className="mt-3 text-center text-2xl font-medium text-gray-900">
             Welcome Back! Please Sign In
           </h2>
           <p className="mt-2 text-center text-sm text-red-600">
